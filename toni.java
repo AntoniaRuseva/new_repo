a5a888f2a579a88c6423bed1c42f8a1b03a6ecb8
AntoniaRuseva <antoniq.r.ruseva@gmail.com>
2023-02-28 14:53:08 +0200

    add java file

---
+++ toni.java
@@ -0,0 +1,13 @@
+
+package main;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 50; i++) {
+
+            System.out.println("Hi");
+        }
+    }
+}
